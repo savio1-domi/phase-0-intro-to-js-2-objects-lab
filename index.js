839c9ff1cb81d27d69bde52f6d7064be632442a6
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-14 18:23:04 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -4,25 +4,19 @@ const employee = {
     streetAddress: "35488825",
 }
 
-// returns an employee with the original key value 
-// pairs and the new key value pair
+/* returns an employee with the original key value 
+   pairs and the new key value pair  */
 function updateEmployeeWithKeyAndValue(employee, key, value){
-
     const newObj = { ...employee }
     newObj[key] = value
     return newObj
-
 }
 updateEmployeeWithKeyAndValue(employee, "sam", "1")
 
-
-
-// does not modify the 
-// original employee (it is non-destructive)
-// and also
-// deletes `key` from a clone of employee and 
-// returns the new employee (it is non-destructive)
-
+/* does not modify the original employee
+   (it is non-destructive) and also deletes 
+   `key` from a clone of employee and returns
+    the new employee (it is non-destructive) */
 function deleteFromEmployeeByKey(employee, key){
     const remEmployee = { ...employee }
     let name = key
@@ -31,14 +25,10 @@ function deleteFromEmployeeByKey(employee, key){
 }
 deleteFromEmployeeByKey(employee, "Juma")
 
-
-
-
 function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
     employee[key] = value;
     return employee
 }
-
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key]
